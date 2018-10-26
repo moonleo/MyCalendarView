e99c4169e58aad29782839e9e7ccd6a419d196f2
moonleo <yanhan_90@126.com>
2018-10-26 22:09:30 +0800

    add for sync @mac

--- app/src/main/java/priv/yh/mycalendar/MyTimePickerDialog.java
+++ app/src/main/java/priv/yh/mycalendar/MyTimePickerDialog.java
@@ -6,9 +6,19 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TimePicker;
 
-public class MyTimePickerDialog extends AlertDialog {
+import java.sql.Time;
+
+public class MyTimePickerDialog extends AlertDialog implements View.OnClickListener{
+
+    private TimePicker onDutyTimePicker;
+    private TimePicker offDutyTimePicker;
+    private Button okBtn;
+    private Button cancelBtn;
+    private OnTimeSetListener mListener;
 
     protected MyTimePickerDialog(Context context) {
         super(context);
@@ -32,6 +42,39 @@ public class MyTimePickerDialog extends AlertDialog {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.time_pick_view);
+        initView();
+        bindEvent();
+    }
+
+    private void initView() {
+        onDutyTimePicker = findViewById(R.id.timepicker_on_duty);
+        offDutyTimePicker = findViewById(R.id.timepicker_off_duty);
+        okBtn = findViewById(R.id.time_picker_ok_btn);
+        cancelBtn = findViewById(R.id.time_picker_cancel_btn);
+    }
+
+    private void bindEvent() {
+        if(okBtn != null) {
+            okBtn.setOnClickListener(this);
+        }
+        if(cancelBtn != null) {
+            cancelBtn.setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.time_picker_ok_btn:
+                mListener.onTimeSet(onDutyTimePicker, onDutyTimePicker.getHour(),
+                        onDutyTimePicker.getMinute(), offDutyTimePicker, offDutyTimePicker.getHour(),
+                        offDutyTimePicker.getMinute());
+                this.dismiss();
+                break;
+            case R.id.time_picker_cancel_btn:
+                this.dismiss();
+                break;
+        }
     }
 
     //We have two time pickers, so we need create a new interface
